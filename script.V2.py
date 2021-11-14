333a05626c687c9ebac702f9927cb8da2b0f2aaa
AbangRombeng <80770646+SUBUR78990@users.noreply.github.com>
2021-11-13 15:05:35 -0900

    Add files via upload

---
+++ script.V2.py
@@ -0,0 +1,25 @@
+clear
+echo " SCRIPT.V2" | lolcat
+echo "╠══════════╡" | lolcat
+echo " ┊,▇▇▇▇▇   ╔═════════════════════════╕" | lolcat
+echo " ┊┊▇▇▇▇▇   ║ Author : Abang-Rombeng " | lolcat
+echo " ┊┊▇▇▇▇▇   ╠═════════════════════════╡" | lolcat
+echo " ▅▅▇▇▇▇▇▅▅ ║ Tggl/Lahir : ##########    " | lolcat
+echo "▕▍╱╲╲╱╱╲   ╚═════════════════════════●" | lolcat
+echo "╭▎▏▍╲╱▍▕      ║ Lokasi : Tangerang    " | lolcat
+echo "┃╯╲＿╱╲＿╱    ╚══════════════════════●" | lolcat
+echo "╰╯┊┊╭╰━━━╮" | lolcat
+echo "┊┳┳╯╰━━━╮┃" | lolcat
+echo "┊┃┃━╯╰━┊╰╯            @HACKERS212" | lolcat
+echo "┊┃╰┳┳┳┳┛                       " | lolcat
+echo "┊╰━┻┻┻┻━╮"  | lolcat
+echo "┊┊┊┊┊┊┊┊┃" 💀💀💀💀💀💀💀💀💀💀💀💀💀 | lolcat
+echo "°════════════════════════════════════" | lolcat
+echo "          KARYA GUA SENDIRI           " | lolcat
+echo "╠════════════════════════════════════╡" | lolcat
+echo "      ANAK DESA BOJONG RENGET           " | lolcat
+echo "╠════════════════════════════════════╡"  | lolcat
+echo
+echo
+
+
